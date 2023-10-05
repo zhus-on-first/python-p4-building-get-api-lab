1c33e9b360605d91788ce07d79f46b7e9ebf03f2
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-10-04 21:27:15 -0700

    lab complete

--- server/app.py
+++ server/app.py
@@ -6,33 +6,59 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakery_list = Bakery.query.all()
+    bakery_list_dict = [bakery.to_dict() for bakery in bakery_list]
+    json_data = jsonify(bakery_list_dict)
+    return make_response(json_data, 200)
 
-@app.route('/bakeries/<int:id>')
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    # bakery = Bakery.query.get(id)
+    session = db.session
+    bakery = session.get(Bakery, id)
 
-@app.route('/baked_goods/by_price')
+    if bakery is None:
+        return make_response({"error": "Bakery not not found"}, 404)
+    
+    bakery_dict = bakery.to_dict()
+    json_data = jsonify(bakery_dict)
+
+    return json_data
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    if goods is None:
+        return make_response({"error": "No baked goods found"}, 404)
+    goods_dict = [good.to_dict() for good in goods]
+    json_data = jsonify(goods_dict)
 
-@app.route('/baked_goods/most_expensive')
+    return make_response(json_data, 200)
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive is None:
+        return make_response({"error": "No baked goods found"}, 404)
+    
+    most_expensive_dict = most_expensive.to_dict()
+    json_data = jsonify(most_expensive_dict)
 
-if __name__ == '__main__':
+    return make_response(json_data, 200)
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,12 +9,34 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Bakery(db.Model, SerializerMixin):
-    __tablename__ = 'bakeries'
+    __tablename__ = "bakeries"
+
+    serialize_rules = ("-baked_goods.bakery", )
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship("BakedGood", back_populates="bakery")
+
+    def __repr__(self):
+        return f"<Bakery ({self.id}) {self.name}>"
 
 class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
+    __tablename__ = "baked_goods"
+
+    serialize_rules = ("-bakery.baked_goods", )
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"))
+
+    bakery = db.relationship("Bakery", back_populates="baked_goods")
+
+    def __repr__(self):
+        return f"<BakedGood ({self.id}) {self.name}>"
